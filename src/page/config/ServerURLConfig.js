0ea876f0f34ebe07e4cf9d1eee9d5c965a083371
stella0306 <starstella22222@gmail.com>
2025-10-30 20:00:35 +0900

    feat: 불필요한 이모지를 제거 및 백엔드 서버 코드 변경으로 URL 주소 변경

--- src/page/config/ServerURLConfig.js
+++ src/page/config/ServerURLConfig.js
@@ -1,17 +1,17 @@
 // 서버 URL 모음
 const serverURLConfig = {
     "cpu": {
-        "cpu_percent_url": "http://localhost:8000/system/api/cpu_percent",
-        "cpu_count_url": "http://localhost:8000/system/api/cpu_count",
+        "cpu_percent_url": "http://localhost:8000/system/cpu/cpu_percent",
+        "cpu_count_url": "http://localhost:8000/system/cpu/cpu_count",
     },
     "memory": {
-        "virtual_memory": "http://localhost:8000/system/api/virtual_memory"
+        "virtual_memory": "http://localhost:8000/system/memory/virtual_memory"
     },
     "network": {
-        "net_io_counters": "http://localhost:8000/system/api/net_io_counters"
+        "net_io_counters": "http://localhost:8000/system/network/net_io_counters"
     },
     "disk": {
-        "virtual_memory": "http://localhost:8000/system/api/virtual_memory" // disk 서버가 개발되기 전까지 임시 URL로 사용함.
+        "virtual_memory": "http://localhost:8000/system/memory/virtual_memory" // disk 서버가 개발되기 전까지 임시 URL로 사용함.
     }
 };
 
