f1dd9a9f00c227c0df653b24dfd44a2f28ac8e0f
stella0306 <starstella22222@gmail.com>
2025-10-31 06:27:00 +0900

    fix: 파일 대소문자 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./page/home/Home.js";
-import GetCPUInfoPage from './page/cpu/GetCpuInfoPage.js';
+import GetCPUInfoPage from './page/cpu/GetCPUInfoPage.js';
 import GetMemoryInfoPage from './page/memory/GetMemoryInfoPage.js';
 import GetNetworkInfoPage from './page/network/GetNetworkInfoPage.js';
 import GetDiskInfoPage from './page/disk/GetDiskInfoPage.js';
